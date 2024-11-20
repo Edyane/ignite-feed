a3dfd5acd622b8d74631af1d963b1d10351daa49
Edyane <edyanealves@gmail.com>
2024-11-20 08:56:51 -0300

    feat: add Sidebar component

---
+++ feed-app/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { PencilLine } from 'phosphor-react';
+import { Avatar } from '../../components/Avatar/Avatar';
+import styles from './Sidebar.module.css';
+import CardBackgroud from '../../assets/code-bg.avif';
+
+export function Sidebar() {
+	return (
+		<aside className={styles.sidebar}>
+            <img className={styles.cover} src={CardBackgroud} alt="" />
+
+            <div className={styles.profile}>
+                <Avatar hasBorder src="https://github.com/edyane.png" />
+                <strong>Edyane</strong>
+                <span>Front-end Developer</span>
+            </div>
+
+            <footer className={styles.footer}>
+                <a href="#">
+                    <PencilLine />
+                    Editar seu perfil
+                </a>
+            </footer>
+
+        </aside>
+	)
+};
