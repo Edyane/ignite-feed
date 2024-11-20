f516d3748764c15848d1bd019c774e170e4b5192
Edyane <edyanealves@gmail.com>
2024-11-19 21:53:23 -0300

    feat: add Comment component

---
+++ feed-app/src/components/Comment/Comment.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Trash } from 'phosphor-react';
+import { ThumbsUp } from 'phosphor-react';
+import { Avatar } from '../Avatar/Avatar';
+import styles from './Comment.module.css';
+
+export function Comment({ content }) {
+    return (
+        <div className={styles.comment}>
+            <Avatar src="https://github.com/lyarkh.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Lucas</strong>
+                            <time title="11 de novembro de 2024" datetime="2024-11-11">
+                                Há 1h atrás
+                            </time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+};
